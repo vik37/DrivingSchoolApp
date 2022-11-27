445e3b0e2a72b7d80ee2908a26d970ff7618f0a8
vik37 <59177404+vik37@users.noreply.github.com>
2022-11-28 00:44:11 +0100

    Add Entities.

---
+++ Services/CategoryServices/DrivingSchoolApp.Services.CategoryServices.ACategoryAPI/DataAccess/CategoryADbContext.cs
@@ -0,0 +1,24 @@
+﻿using DrivingSchoolApp.Services.CategoryServices.ACategoryAPI.DataAccess.Entities;
+using DrivingSchoolApp.Services.CategoryServices.ACategoryAPI.DataAccess.Etities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrivingSchoolApp.Services.CategoryServices.ACategoryAPI.DataAccess
+{
+	public class CategoryADbContext : DbContext
+	{
+		public CategoryADbContext(DbContextOptions option) : base(option)
+		{ }
+
+		public DbSet<CategoryA> CategoryA { get; set; }
+		public DbSet<Instructor> Instructor { get; set; }
+		public DbSet<Motorcycle> Motorcycle { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<CategoryA>()
+				.HasMany(x => x.Instructors)
+				.WithOne(x => x.CategoryA)
+				.HasForeignKey(x => x.CategoryAId);			
+		}
+	}
+}
---
+++ Services/CategoryServices/DrivingSchoolApp.Services.CategoryServices.ACategoryAPI/DataAccess/Entities/CategoryA.cs
@@ -0,0 +1,20 @@
+﻿using DrivingSchoolApp.Services.CategoryServices.ACategoryAPI.DataAccess.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using DrivingSchoolApp.Services.CategoryServices.ACategoryAPI.DataAccess.Entities.Enum;
+
+namespace DrivingSchoolApp.Services.CategoryServices.ACategoryAPI.DataAccess.Etities
+{
+	public class CategoryA : Base
+	{		
+		public string City { get; set; }
+		public string Address { get; set; }
+		public string Phone { get; set; }
+		public TypeOfClass TypeOfClass { get; set; }
+		public double Price { get; set; }
+		public string Photo { get; set; }
+		public string Description { get; set; }
+		public IEnumerable<Instructor> Instructors { get; set; } = new List<Instructor>();
+		public IEnumerable<Motorcycle> Motorcycles { get; set; } = new List<Motorcycle>();
+	}
+}
