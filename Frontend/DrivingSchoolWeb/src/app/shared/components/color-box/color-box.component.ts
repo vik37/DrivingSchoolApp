ae2660c0f70ffe815c3e91bbcd752f8bd237a4c9
vik37 <59177404+vik37@users.noreply.github.com>
2023-01-09 19:06:39 +0100

    Add color-box for color data in Category A.

---
+++ Frontend/DrivingSchoolWeb/src/app/shared/components/color-box/color-box.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-color-box',
+  templateUrl: './color-box.component.html',
+  styleUrls: ['./color-box.component.css']
+})
+export class ColorBoxComponent implements OnChanges {
+  @Input() colorText: string = '';
+  colors:string[] = [];
+  constructor() { }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(changes['colorText'].firstChange){
+      this.colors = this.colorText.split("&").map(color => color.trim().toLowerCase());
+      console.log('color text',this.colors)
+    }
+  }
+
+}
